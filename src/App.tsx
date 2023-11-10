aefbfe95ac3201b9e86a1770ff675904a1b0eccc
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-11-10 20:47:11 -0300

    Criando ambiente para aparecer grid e criando types para processo de mostrar items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { Container, GridArea, Info, InfoArea, LogoLink } from "./App.styles"
+import { Container, Grid, GridArea, Info, InfoArea, LogoLink } from "./App.styles"
 import logo from './assets/devmemory_logo.png'
 import RestartIcon from './svgs/restart.svg'
 import { Button } from "./components/Button"
 import { InfoItem } from "./components/InfoItem"
+import { useEffect, useState } from "react"
+import { GridItem } from "./types/GridItem"
 
 function App() {
+
+  const [playing, setPlaying] = useState(false)
+  const [timeElapsed, setTimeElapsed] = useState(0)
+  const [moveCount, setMoveCount] = useState(0)
+  const [shownCount, setShownCount] = useState(0)
+  const [gridItems, setGridItems] = useState<GridItem[]>([])
+
+  useEffect(() => {
+    resetAndCreateGrid()
+  }, [])
 
   const resetAndCreateGrid = () => {
 
@@ -24,7 +36,9 @@ function App() {
         <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
       </Info>
       <GridArea>
-        ...
+        <Grid>
+
+        </Grid>
       </GridArea>
     </Container>
   )
